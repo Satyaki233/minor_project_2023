8328fd34e038e4d45804c2ea25976c8aa0f11013
Satyaki De Sarkar <satyakidesarkar2017@gmail.com>
2023-09-25 22:32:32 +0530

    The project is complete with static images

--- image_entropy.py
+++ image_entropy.py
@@ -4,9 +4,8 @@ from PIL import Image
 
 
 class Entropy:
-    def __init__(self,filename:str="entropy_dataset.txt"):
-        self.filename = filename
-
+    def __init__(self,path:str):
+        self.path = path
     def shanon_entropy(self,arr: np.array) -> float:
         _, counts = np.unique(arr, return_counts=True)
         probabilities = counts / len(arr)
@@ -28,16 +27,17 @@ class Entropy:
             prev_y = 0
         return np.array(list1)
 
-    def entropy_image(self,count: int) -> np.array:
-        img = Image.open("images/" + str(count) + ".pgm")
+    def entropy_image(self,file_name: str) -> np.array:
+        img = Image.open(file_name)
         img_arr = np.asarray(img)
         en = self.entropy_array(img_arr, 5)
         return en
 
-    def save_to_file(self,en_arr: np.array, count: int):
-        file = open(self.filename , 'a')
-        
-        file.write("IMG-" + str(count))
+    def save_to_file(self,en_arr: np.array, file_name:str):
+        entopy_file="entropy.txt"
+        file = open(entopy_file, 'a')
+
+        file.write(file_name)
         file.write("\n")
         for i in en_arr:
             file.write(str(i) + ",")
@@ -45,43 +45,77 @@ class Entropy:
 
         file.close()
 
-    def read_to_file(self) -> tuple:
+    def read_to_file(self) -> tuple[np.array,np.array]:
         key = []
         value = []
-        file = open(self.filename, 'r')
+        file = open('entropy.txt', 'r')
         count = 0
         k = ""
         v = ""
         while True:
             k = str(file.readline())
             v = str(file.readline())
-
+            temp=[]
             if not k:
                 break
             else:
                 key.append(k[:len(k) - 1])
-                value.append(list(v[:len(v) - 2].split(",")))
-                
+                for i in list(v[:len(v) - 2].split(",")):
+                    temp.append(float(i))
+                value.append(temp)
 
         file.close()
-        return (key, value)
+        return (np.array(key), np.array(value))
+
+    def image_entropy(self,path:str,folder:str=None):
+        list_of_file=os.listdir(path+folder)
+        for name in list_of_file:
+            en_arr = self.entropy_image(path+folder+name)
+            self.save_to_file(en_arr=en_arr,file_name=folder)
 
 
-def main_2():
-    folder = os.listdir("./images")
-    count = len(folder)
-    i = 0
-    obj=Entropy()
-    while i < count:
-        en_arr = obj.entropy_image(i)
+
+
+
+def calculate_entropy():
+    if os.path.exists("entropy.txt"):
+        os.remove("entropy.txt")
+    path = "./images/"
+    list_of_file_name = os.listdir(path)
+
+    obj=Entropy(path)
+    if os.path.exists("entropy.txt"):
+        os.remove("entropy.txt")
+        
+    for name in list_of_file_name:
+        en_arr = obj.entropy_image(path+name)
         # print(en_arr, en_arr.shape)
-        obj.save_to_file(en_arr=en_arr, count=i)
-        i += 1
+        obj.save_to_file(en_arr=en_arr, file_name=name)
     key, value = obj.read_to_file()
-    print(key)
+    print(type(key))
     for i in range(len(key)):
         print(key[i])
-        print(value[i])
+        print(len(value[i]))
+
+def calculate_entropy_02():
+    if os.path.exists("entropy.txt"):
+        os.remove("entropy.txt")
+    path="./database/face_database/"
+    obj = Entropy(path=path)
+    x = os.listdir(path)
+    for i in x:
+        
+        if "s" in i:
+            y=os.listdir(path+i)
+            for j in y:
+                en_arr = obj.entropy_image(path+i+'/'+j)
+                obj.save_to_file(en_arr=en_arr,file_name=i)
+
+def get_entropy():
+    en = Entropy("./face_database/")
+    return en.read_to_file()
+
 
 if __name__ == "__main__":
-    main_2()
+    calculate_entropy_02()
+    
